98b761d2c62834a07a6af2569db08745031e7013
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-09-10 04:17:54 -0400

    Update CreditsPopUp.hx

--- source/CreditsPopUp.hx
+++ source/CreditsPopUp.hx
@@ -46,19 +46,23 @@ class CreditsPopUp extends FlxSpriteGroup
 			songCreator = 'roblox2aj';
 		  case 'binomial':
 			songCreator = 'amgunt';
+		  case 'egg-torsion':
+			songCreator = null;
 		}
 		switch (PlayState.storyWeek)
 		{
-			case 0 | 1:
+			case 0 
 				headingPath = {path: 'songHeadings/stremblixHeading', antiAliasing: false, iconOffset: 0};
-			case 2:
+			case 1:
 				headingPath = {path: 'songHeadings/hazetalHeading', antiAliasing: true, iconOffset: 0};
-			case 3:
+			case 2:
 				headingPath = {path: 'songHeadings/zlixHeading', antiAliasing: false, iconOffset: 0};
-			case 4:
+			case 3:
 				headingPath = {path: 'songHeadings/roblox2ajHeading', antiAliasing: true, iconOffset: 0};
-			case 5:
+			case 4:
 				headingPath = {path: 'songHeadings/amguntHeading', antiAliasing: false, iconOffset: 0};
+			case 5:
+                		bg = new FlxSprite().makeGraphic(400, 50, FlxColor.WHITE);
         	}
 		switch (PlayState.SONG.song.toLowerCase())
 		{
