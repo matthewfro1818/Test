eee341ddbe416fcea59eda96130a4908793a3bf7
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-09-10 08:09:22 -0400

    Update ExtrasMenuState.hx

--- source/ExtrasMenuState.hx
+++ source/ExtrasMenuState.hx
@@ -270,7 +270,7 @@ class ExtrasMenuState extends MusicBeatState
 				selectedSomethin = true;
 				FlxG.mouse.visible = false;
 				FlxG.sound.play(Paths.sound('cancelMenu'));
-				MusicBeatState.switchState(new TitleState());
+				MusicBeatState.switchState(new states.MainMenuState());
 			}
 
 			if (controls.ACCEPT || (FlxG.mouse.overlaps(menuItems, FlxG.camera) && FlxG.mouse.justPressed && allowMouse))
