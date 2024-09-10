a524ca9ea550f23b4cf0949ec665cba335872bc1
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-09-10 05:36:33 -0400

    Update FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -70,7 +70,47 @@ class FreeplayState extends MusicBeatState
 			{
 				FlxTransitionableState.skipNextTransIn = true;
 				persistentUpdate = false;
-				MusicBeatState.switchState(new states.ErrorState("NO WEEKS ADDED FOR FREEPLAY\n\nPress " + accept + " to go to the Week Editor Menu.\nPress " + reject + " to return to Main Menu.",
+				MusicBeatState.switchState(new states.ErrorState("NO WEEKS ADDED FOR FREEPLAY\n\nPress ACCEPT to go to the Week Editor Menu.\nPress BACK to return to Main Menu.",
+					function() MusicBeatState.switchState(new states.editors.WeekEditorState()),
+					function() MusicBeatState.switchState(new states.MainMenuState())));
+				return;
+			}
+		if(WeekData.weeksList.length < 1 && PlayState.isUniverse)
+			{
+				PlayState.isUniverse = true;
+				FlxTransitionableState.skipNextTransIn = true;
+				persistentUpdate = false;
+				MusicBeatState.switchState(new states.ErrorState("NO WEEKS ADDED FOR FREEPLAY\n\nPress ACCEPT to go to the Week Editor Menu.\nPress BACK to return to Main Menu.",
+					function() MusicBeatState.switchState(new states.editors.WeekEditorState()),
+					function() MusicBeatState.switchState(new states.MainMenuState())));
+				return;
+			}
+		if(WeekData.weeksList.length < 1 && PlayState.isGolden)
+			{
+				PlayState.isGolden = true;
+				FlxTransitionableState.skipNextTransIn = true;
+				persistentUpdate = false;
+				MusicBeatState.switchState(new states.ErrorState("NO WEEKS ADDED FOR FREEPLAY\n\nPress ACCEPT to go to the Week Editor Menu.\nPress BACK to return to Main Menu.",
+					function() MusicBeatState.switchState(new states.editors.WeekEditorState()),
+					function() MusicBeatState.switchState(new states.MainMenuState())));
+				return;
+			}
+		if(WeekData.weeksList.length < 1 && PlayState.isDaveAndBambi)
+			{
+				PlayState.isDaveAndBambi = true;
+				FlxTransitionableState.skipNextTransIn = true;
+				persistentUpdate = false;
+				MusicBeatState.switchState(new states.ErrorState("NO WEEKS ADDED FOR FREEPLAY\n\nPress ACCEPT to go to the Week Editor Menu.\nPress BACK to return to Main Menu.",
+					function() MusicBeatState.switchState(new states.editors.WeekEditorState()),
+					function() MusicBeatState.switchState(new states.MainMenuState())));
+				return;
+			}
+		if(WeekData.weeksList.length < 1 && PlayState.isSecret)
+			{
+				PlayState.isSecret = true;
+				FlxTransitionableState.skipNextTransIn = true;
+				persistentUpdate = false;
+				MusicBeatState.switchState(new states.ErrorState("NO WEEKS ADDED FOR FREEPLAY\n\nPress ACCEPT to go to the Week Editor Menu.\nPress BACK to return to Main Menu.",
 					function() MusicBeatState.switchState(new states.editors.WeekEditorState()),
 					function() MusicBeatState.switchState(new states.MainMenuState())));
 				return;
