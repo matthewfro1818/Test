7b7310067065da3a00b9502aace6b7a4a2a3b7ea
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-10-06 07:18:34 -0400

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -39,8 +39,6 @@ class MainMenuState extends MusicBeatState
 	var magenta:FlxSprite;
 	var camFollow:FlxObject;
 
-	var menuCharacter:FlxSprite;
-
 	override function create()
 	{
 		#if MODS_ALLOWED
@@ -147,9 +145,9 @@ class MainMenuState extends MusicBeatState
 		optionsbutton.flipX = false; //You should have already animated it in the right position in Animate
 		add(optionsbutton);
 
-		menuCharacter = new FlxSprite(238, 199).loadGraphic(Paths.image('backgrounds/$name'));
-		menuCharacter.scrollFactor.set(0, 0);
-		add(menuCharacter);
+	        var mchar:FlxSprite = new FlxSprite(238, 199).loadGraphic(Paths.image('backgrounds/$name'));
+		mchar.scrollFactor.set(0, 0);
+		add(mchar);
 	}
 
 	var selectedSomethin:Bool = false;
